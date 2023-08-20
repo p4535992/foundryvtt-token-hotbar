4075ef49023f7f10dd586d0834d48d76e6cbc54d
p4535992 <tentimarco0@gmail.com>
2023-08-20 08:35:33 +0200

    version 1.5.0

--- src/scripts/api.mjs
+++ src/scripts/api.mjs
@@ -1,13 +1,13 @@
 import { saveHotbar, shareHotbar } from "./features.mjs";
 
 const API = {
-	saveHotbar: async function (hotbarToStore, documentWithHotbar, entity) {
-		await saveHotbar(hotbarToStore, documentWithHotbar, entity);
-	},
-	// Replace `game.user` with `game.users.get(otherUserId)` to share another
-	// user's hotbar instead of yours.
-	shareHotbar: async function (userId) {
-		await shareHotbar(userId);
-	}
+  saveHotbar: async function (hotbarToStore, documentWithHotbar, entity) {
+    await saveHotbar(hotbarToStore, documentWithHotbar, entity);
+  },
+  // Replace `game.user` with `game.users.get(otherUserId)` to share another
+  // user's hotbar instead of yours.
+  shareHotbar: async function (userId) {
+    await shareHotbar(userId);
+  },
 };
 export default API;
--- src/scripts/lib/lib.mjs
+++ src/scripts/lib/lib.mjs
@@ -7,47 +7,47 @@ import CONSTANTS from "../constants.mjs";
 
 // 0 = none, warnings = 1, debug = 2, all = 3
 export function debug(msg, args = "") {
-	if (game.settings.get(CONSTANTS.MODULE_NAME, "debug")) {
-		console.log(`DEBUG | ${CONSTANTS.MODULE_NAME} | ${msg}`, args);
-	}
-	return msg;
+  if (game.settings.get(CONSTANTS.MODULE_NAME, "debug")) {
+    console.log(`DEBUG | ${CONSTANTS.MODULE_NAME} | ${msg}`, args);
+  }
+  return msg;
 }
 export function log(message) {
-	message = `${CONSTANTS.MODULE_NAME} | ${message}`;
-	console.log(message.replace("<br>", "\n"));
-	return message;
+  message = `${CONSTANTS.MODULE_NAME} | ${message}`;
+  console.log(message.replace("<br>", "\n"));
+  return message;
 }
 export function notify(message) {
-	message = `${CONSTANTS.MODULE_NAME} | ${message}`;
-	ui.notifications?.notify(message);
-	console.log(message.replace("<br>", "\n"));
-	return message;
+  message = `${CONSTANTS.MODULE_NAME} | ${message}`;
+  ui.notifications?.notify(message);
+  console.log(message.replace("<br>", "\n"));
+  return message;
 }
 export function info(info, notify = false) {
-	info = `${CONSTANTS.MODULE_NAME} | ${info}`;
-	if (notify) ui.notifications?.info(info);
-	console.log(info.replace("<br>", "\n"));
-	return info;
+  info = `${CONSTANTS.MODULE_NAME} | ${info}`;
+  if (notify) ui.notifications?.info(info);
+  console.log(info.replace("<br>", "\n"));
+  return info;
 }
 export function warn(warning, notify = false) {
-	warning = `${CONSTANTS.MODULE_NAME} | ${warning}`;
-	if (notify) ui.notifications?.warn(warning);
-	console.warn(warning.replace("<br>", "\n"));
-	return warning;
+  warning = `${CONSTANTS.MODULE_NAME} | ${warning}`;
+  if (notify) ui.notifications?.warn(warning);
+  console.warn(warning.replace("<br>", "\n"));
+  return warning;
 }
 export function error(error, notify = true) {
-	error = `${CONSTANTS.MODULE_NAME} | ${error}`;
-	if (notify) ui.notifications?.error(error);
-	return new Error(error.replace("<br>", "\n"));
+  error = `${CONSTANTS.MODULE_NAME} | ${error}`;
+  if (notify) ui.notifications?.error(error);
+  return new Error(error.replace("<br>", "\n"));
 }
 export function timelog(message) {
-	warn(Date.now(), message);
+  warn(Date.now(), message);
 }
 export const i18n = (key) => {
-	return game.i18n.localize(key)?.trim();
+  return game.i18n.localize(key)?.trim();
 };
 export const i18nFormat = (key, data = {}) => {
-	return game.i18n.format(key, data)?.trim();
+  return game.i18n.format(key, data)?.trim();
 };
 // export const setDebugLevel = (debugText: string): void => {
 //   debugEnabled = { none: 0, warn: 1, debug: 2, all: 3 }[debugText] || 0;
@@ -55,7 +55,7 @@ export const i18nFormat = (key, data = {}) => {
 //   if (debugEnabled >= 3) CONFIG.debug.hooks = true;
 // };
 export function dialogWarning(message, icon = "fas fa-exclamation-triangle") {
-	return `<p class="${CONSTANTS.MODULE_NAME}-dialog">
+  return `<p class="${CONSTANTS.MODULE_NAME}-dialog">
         <i style="font-size:3rem;" class="${icon}"></i><br><br>
         <strong style="font-size:1.2rem;">${CONSTANTS.MODULE_NAME}</strong>
         <br><br>${message}
@@ -63,34 +63,34 @@ export function dialogWarning(message, icon = "fas fa-exclamation-triangle") {
 }
 // =========================================================================================
 export function getActor(target) {
-	if (target instanceof Actor) return target;
-	if (stringIsUuid(target)) {
-		target = fromUuidSync(target);
-	}
-	target = getDocument(target);
-	return target?.actor ?? target;
+  if (target instanceof Actor) return target;
+  if (stringIsUuid(target)) {
+    target = fromUuidSync(target);
+  }
+  target = getDocument(target);
+  return target?.actor ?? target;
 }
 
 export function getToken(documentUuid) {
-	const document = fromUuidSync(documentUuid);
-	return document instanceof TokenDocument ? document.object : document;
+  const document = fromUuidSync(documentUuid);
+  return document instanceof TokenDocument ? document.object : document;
 }
 
 export function getDocument(target) {
-	if (stringIsUuid(target)) {
-		target = fromUuidSync(target);
-	}
-	return target?.document ?? target;
+  if (stringIsUuid(target)) {
+    target = fromUuidSync(target);
+  }
+  return target?.document ?? target;
 }
 
 export function stringIsUuid(inId) {
-	return typeof inId === "string" && (inId.match(/\./g) || []).length && !inId.endsWith(".");
+  return typeof inId === "string" && (inId.match(/\./g) || []).length && !inId.endsWith(".");
 }
 
 export function getUuid(target) {
-	if (stringIsUuid(target)) return target;
-	const document = getDocument(target);
-	return document?.uuid ?? false;
+  if (stringIsUuid(target)) return target;
+  const document = getDocument(target);
+  return document?.uuid ?? false;
 }
 
 /**
@@ -100,13 +100,13 @@ export function getUuid(target) {
  * @returns {Array<Token>}
  */
 export function getTokensAtLocation(position) {
-	const tokens = [...canvas.tokens.placeables].filter((token) => token.mesh.visible);
-	return tokens.filter((token) => {
-		return (
-			position.x >= token.x &&
-			position.x < token.x + token.document.width * canvas.grid.size &&
-			position.y >= token.y &&
-			position.y < token.y + token.document.height * canvas.grid.size
-		);
-	});
+  const tokens = [...canvas.tokens.placeables].filter((token) => token.mesh.visible);
+  return tokens.filter((token) => {
+    return (
+      position.x >= token.x &&
+      position.x < token.x + token.document.width * canvas.grid.size &&
+      position.y >= token.y &&
+      position.y < token.y + token.document.height * canvas.grid.size
+    );
+  });
 }
